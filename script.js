7dd92b286b06d162984369366d55cea497bba73a
Luara <luaraap7@gmail.com>
2022-09-04 19:40:44 -0300

    semana2ok

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
 //Escritoras Negras
 
+const escritora1 = "Carolina Maria de Jesus".toUpperCase();
+const anoDeNascimento1 = 1914;
+const morouEmSaoPaulo1 = true;
+const primeiroLançamento1 = ["Quarto de Despejo: Diário de uma Favelada (1960"];
+console.log(escritora1, "Ano de Nascimento:", anoDeNascimento1, "Morou em São Paulo:", morouEmSaoPaulo1, "Primeiro Lançamento:", primeiroLançamento1);
+
+const escritora2 = "Conceição Evaristo".toUpperCase();
+const anoDeNascimento2 = 1946;
+const morouEmSaoPaulo2 = false;
+const primeiroLançamento2 = ["Ponciá Vicêncio - Becos da Memória (2006)"];
+console.log(escritora2, "Ano de Nascimento:", anoDeNascimento2, "Morou em São Paulo:", morouEmSaoPaulo2, "Primeiro Lançamento:", primeiroLançamento2);
+
+
+const escritora3 = "Ryane Leão".toUpperCase();
+const anoDeNascimento3 = 1989;
+const morouEmSaoPaulo3 = true;
+const primeiroLançamento3 = ["Tudo Nela Brilha e Queima (2017)"];
+console.log(escritora3, "Ano de Nascimento:", anoDeNascimento3, "Morou em São Paulo:", morouEmSaoPaulo3, "Primeiro Lançamento:", primeiroLançamento3);
+
+
+// const media = (anoDeNascimento1 + anoDeNascimento2 +anoDeNascimento3)/3;
+// console.log(media);
+
+// const morouEmSaoPaulo = morouEmSaoPaulo1 && morouEmSaoPaulo2 && morouEmSaoPaulo3;
+// console.log(morouEmSaoPaulo);
+
