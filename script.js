57e92b863177f7c6a05ad6c676d9162da063476e
Luara <luaraap7@gmail.com>
2022-09-04 21:00:07 -0300

    semana3ok

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 //Escritoras Negras
 
-const escritora1 = "Carolina Maria de Jesus".toUpperCase();
-const anoDeNascimento1 = 1914;
-const morouEmSaoPaulo1 = true;
-const primeiroLançamento1 = ["Quarto de Despejo: Diário de uma Favelada (1960"];
-console.log(escritora1, "Ano de Nascimento:", anoDeNascimento1, "Morou em São Paulo:", morouEmSaoPaulo1, "Primeiro Lançamento:", primeiroLançamento1);
+// const escritora1 = "Carolina Maria de Jesus".toUpperCase();
+// const anoDeNascimento1 = 1914;
+// const morouEmSaoPaulo1 = true;
+// const primeiroLançamento1 = ["Quarto de Despejo: Diário de uma Favelada (1960"];
+// console.log(escritora1, "Ano de Nascimento:", anoDeNascimento1, "Morou em São Paulo:", morouEmSaoPaulo1, "Primeiro Lançamento:", primeiroLançamento1);
 
-const escritora2 = "Conceição Evaristo".toUpperCase();
-const anoDeNascimento2 = 1946;
-const morouEmSaoPaulo2 = false;
-const primeiroLançamento2 = ["Ponciá Vicêncio - Becos da Memória (2006)"];
-console.log(escritora2, "Ano de Nascimento:", anoDeNascimento2, "Morou em São Paulo:", morouEmSaoPaulo2, "Primeiro Lançamento:", primeiroLançamento2);
+// const escritora2 = "Conceição Evaristo".toUpperCase();
+// const anoDeNascimento2 = 1946;
+// const morouEmSaoPaulo2 = false;
+// const primeiroLançamento2 = ["Ponciá Vicêncio - Becos da Memória (2006)"];
+// console.log(escritora2, "Ano de Nascimento:", anoDeNascimento2, "Morou em São Paulo:", morouEmSaoPaulo2, "Primeiro Lançamento:", primeiroLançamento2);
 
 
-const escritora3 = "Ryane Leão".toUpperCase();
-const anoDeNascimento3 = 1989;
-const morouEmSaoPaulo3 = true;
-const primeiroLançamento3 = ["Tudo Nela Brilha e Queima (2017)"];
-console.log(escritora3, "Ano de Nascimento:", anoDeNascimento3, "Morou em São Paulo:", morouEmSaoPaulo3, "Primeiro Lançamento:", primeiroLançamento3);
+// const escritora3 = "Ryane Leão".toUpperCase();
+// const anoDeNascimento3 = 1989;
+// const morouEmSaoPaulo3 = true;
+// const primeiroLançamento3 = ["Tudo Nela Brilha e Queima (2017)"];
+// console.log(escritora3, "Ano de Nascimento:", anoDeNascimento3, "Morou em São Paulo:", morouEmSaoPaulo3, "Primeiro Lançamento:", primeiroLançamento3);
 
 
 // const media = (anoDeNascimento1 + anoDeNascimento2 +anoDeNascimento3)/3;
@@ -26,3 +26,36 @@ console.log(escritora3, "Ano de Nascimento:", anoDeNascimento3, "Morou em São P
 // const morouEmSaoPaulo = morouEmSaoPaulo1 && morouEmSaoPaulo2 && morouEmSaoPaulo3;
 // console.log(morouEmSaoPaulo);
 
+escritora1 = {
+    nome: "Carolina Maria de Jesus",
+    anoDeNascimento: 1914,
+    morouEmSaoPaulo: true,
+
+},
+
+escritora2 = {
+    nome2: "Conceição Evaristo",
+    anoDeNascimento: 1946,
+    morouEmSaoPaulo: false,
+},
+
+escritora3 = {
+    nome: "Ryane Leão",
+    anoDeNascimento: 1989,
+    morouEmSaoPaulo: false,
+}
+
+const escritoraNegra1 = []
+
+escritoraNegra1.push(escritora1);
+console.log(escritoraNegra1);
+
+const escritoraNegra2 = []
+
+escritoraNegra2.push(escritora2);
+console.log(escritoraNegra2);
+
+const escritoraNegra3 = []
+
+escritoraNegra3.push(escritora3);
+console.log(escritoraNegra3);
